d6b3a3103df5035210b29622e5886a7b867e3bff
Jesús Alonso Solís Mundaca <jsolis@belatrix.com>
2018-12-04 12:31:39 -0500

    configure routes, refactoring

--- app/containers/LinkListContainer/actions.js
+++ app/containers/LinkListContainer/actions.js
@@ -1,4 +1,8 @@
-import { REQUEST_LINKS_SUCCEEDED, REQUEST_LINKS_FAILED } from './constants';
+import {
+  REQUEST_LINKS_SUCCEEDED,
+  REQUEST_LINKS_FAILED,
+  REQUEST_LINKS
+} from './constants';
 
 export function requestLinksSucceeded(links) {
   return {
@@ -11,5 +15,12 @@ export function requestLinksFailed(message) {
   return {
     type: REQUEST_LINKS_FAILED,
     message,
+  };
+}
+
+export function requestLinks(topicName) {
+  return {
+    type: REQUEST_LINKS,
+    topicName,
   };
 }
--- app/containers/LinkListContainer/selectors.js
+++ app/containers/LinkListContainer/selectors.js
@@ -1,5 +1,6 @@
 import { createSelector } from 'reselect';
 import { initialState } from './reducer';
+import selectNavigationContainer from '../NavigationContainer/selectors';
 
 /**
  * Direct selector to the linkListContainer state domain
@@ -12,12 +13,25 @@ const selectLinkListContainerDomain = state =>
  * Other specific selectors
  */
 
+const selectRouteTopic = () => (state, props) => {
+  return props.match.params.topicName;
+};
+const selectTopic = () => createSelector(
+  selectNavigationContainer(),
+  selectRouteTopic(),
+  (navigationState, routeTopicName) => {
+    const selectedTopic = navigationState.topics.find(t => t.name === routeTopicName);
+    return selectedTopic || {name: ''};
+  }
+);
 /**
  * Default selector used by LinkListContainer
  */
-
-const makeSelectLinkListContainer = () =>
-  createSelector(selectLinkListContainerDomain, substate => substate.toJS());
+const makeSelectLinkListContainer = () => createSelector(
+  selectLinkListContainerDomain,
+  selectTopic(),
+  (substate, topic) => Object.assign(substate.toJS(), {topicName: topic.name}),
+);
 
 export default makeSelectLinkListContainer;
 export { selectLinkListContainerDomain };
